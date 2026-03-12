cb695b9b41e353fa45abd24375318fe34c372887
sneakyginger <gregor.vanv@gmail.com>
2026-03-12 18:32:41 +0100

    2 and 4 component dispensing fixed
    loading bar quicker, removed redundancy

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -26,7 +26,7 @@ def create_text(text, position, color=(255,255,255)):
     surface_text_rect = surface_text.get_rect()
     surface_text_rect.center = position
     return surface_text, surface_text_rect
-work = 10000000
+work = 5000000
 def doWork():
     global loading_progress
     for ii in range(work):
@@ -58,19 +58,19 @@ mengen_bezig, mengen_bezig_rect = create_text("Aan het mengen", (width // 2, hei
 #Text menu 0
 menu0_text, menu0_text_rect = create_text("START", (width // 2, 100), (0,0,0))
 #Text menu 1
-menu1_text, menu1_text_rect = create_text("2 component dispensing", (width // 2, 100), (255,255,255))
+menu1_text, menu1_text_rect = create_text("2 component dispensing", (width // 2, 100), (0,0,0))
 #Text menu 2
-menu2_text, menu2_text_rect = create_text("4 component dispensing", (width //2, 100))
+menu2_text, menu2_text_rect = create_text("4 component dispensing", (width //2, 100), (0,0,0))
 #Text menu 3
-menu3_text, menu3_text_rect = create_text("4 component dispensing", (width // 2, 100), (255,255,255))
+menu3_text, menu3_text_rect = create_text("4 component dispensing", (width // 2, 100), (0,0,0))
 #Text menu 4
-menu4_text, menu4_text_rect = create_text("Mixing", (width // 2, 100), (255,255,255))
+menu4_text, menu4_text_rect = create_text("Would you like to start mixing?", (width // 2, 100), (0,0,0))
 #Text menu 5
-menu5_text, menu5_text_rect = create_text("Settings", (width // 2, 100), (255,255,255))
+menu5_text, menu5_text_rect = create_text("Settings", (width // 2, 100), (0,0,0))
 #Text menu 6
-menu6_text, menu6_text_rect = create_text("Mixing Settings", (width // 2, 100), (255,255,255))
+menu6_text, menu6_text_rect = create_text("Mixing Settings", (width // 2, 100), (0,0,0))
 #Text menu 7
-menu7_text, menu7_text_rect = create_text("Replace cartridge", (width // 2, 100), (255,255,255))
+menu7_text, menu7_text_rect = create_text("Replace cartridge", (width // 2, 100), (0,0,0))
 
 
 
@@ -86,9 +86,13 @@ return_image, return_image_rect = load_image(r'./Sprites/return.png', (100, 100)
 loading_bar_image, loading_bar_image_rect = load_image(r'./Sprites/white.png',(8,150) ,(200, height//2))
 loading_progress = 0
 loading_bar_width = 8
-weight_bar_image, weight_bar_image_rect = load_image(r'./Sprites/white.png',(8,150) ,(200, height//2))
+weight_bar_image, weight_bar_image_rect = load_image(r'./Sprites/black.png',(8,150) ,(200, height//2))
 weight_progress = 50
 weight_bar_width = 8
+hardness_bar_image, hardness_bar_image_rect = load_image(r'./Sprites/black.png',(8,150) ,(200, height//2))
+hardness_progress = 25
+hardness_bar_width = 8
+
 
 #load button sprites to test
 button1_image, button1_image_rect = load_image(r'./Sprites/button.png', (100, 100), (loci[0]))
@@ -104,9 +108,8 @@ no_image, no_image_rect = load_image(r'./Sprites/no.png', (100, 100), (loci[2]))
 
 running = True
 while running:
-    Clock = time.time()
     selection_image_rect.center = (loci[location]) 
-
+    screen.fill((255, 255, 255))          # clear screen (white background)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -124,7 +127,19 @@ while running:
                         location = 4
 
                 elif menu == 2:
-                    location = available_locations(location, "right", [1,2,4])
+                    if weight_progress < 100:
+                        location  = 0
+                        weight_progress += 1
+                    else:
+                        weight_progress = 100
+                        location = 4
+                elif menu == 3:
+                    if weight_progress > 50:
+                        location  = 0
+                        hardness_progress += 1
+                    else:
+                        hardness_progress = 0
+                        location = 4
                 elif menu == 4:
                     location = available_locations(location, "right", [1,2,4])
                 elif menu == 5:  
@@ -143,7 +158,19 @@ while running:
                         weight_progress = 0
                         location = 4
                 elif menu == 2:
-                    location = available_locations(location, "left", [1,2,4])
+                    if weight_progress > 0:
+                        location  = 0
+                        weight_progress -= 1
+                    else:
+                        weight_progress = 0
+                        location = 4
+                elif menu == 3:
+                    if weight_progress > 0:
+                        location  = 0
+                        hardness_progress -= 1
+                    else:
+                        hardness_progress = 0
+                        location = 4
                 elif menu == 5:
                     location = available_locations(location, "left", [1,2,4])
 
@@ -197,7 +224,6 @@ while running:
 
                 
     if menu == 0:
-        screen.fill((255, 255, 255))          # clear screen (white background)
         screen.blit(menu0_text, menu0_text_rect)  # draw menu text in the center of the screen
         screen.blit(selection_image, selection_image_rect)  # draw cursor
         screen.blit(settings_image, settings_image_rect)  # draw settings image
@@ -206,45 +232,51 @@ while running:
         screen.blit(button2_image, button2_image_rect)  # draw button 2
         screen.blit(button3_image, button3_image_rect)  # draw button 3
     if menu == 1:
-        screen.fill((255, 0, 0))          # clear screen (red background)
         screen.blit(menu1_text, menu1_text_rect)  # draw menu text in the center of the screen
         screen.blit(return_image, return_image_rect)  # draw return image in bottom right corner
         if location == 4:
             screen.blit(selection_image, selection_image_rect)  # draw cursor
         weight_bar_width = abs(weight_progress)*width/2//100
-        weight_bar_image = pygame.transform.scale(weight_bar_image, (int(weight_bar_width), 50))  # scale loading bar based on selected weight
-        weight_bar_image_rect = weight_bar_image.get_rect(midleft=(200, 3/4*height))  # update loading bar position
-        screen.blit(weight_bar_image, weight_bar_image_rect)  # draw loading bar
-        weight_text,weight_rect = create_text("Desired weight: {weight_progress} g", (width // 2, height // 2), (255,255,255))
+        weight_bar_image_use = pygame.transform.scale(weight_bar_image, (int(weight_bar_width), 50))  # scale loading bar based on selected weight
+        weight_bar_image_use_rect = weight_bar_image_use.get_rect(midleft=(200, 3/4*height))  # update loading bar position
+        screen.blit(weight_bar_image_use, weight_bar_image_use_rect)  # draw loading bar
+        weight_text,weight_rect = create_text(f"Desired weight: {weight_progress} g", (width // 2, height // 2), (0,0,0))
         screen.blit(weight_text, weight_rect)  # draw weight text in the center
     if menu == 2:
-        screen.fill((0, 255, 0))          # clear screen (green background)
         screen.blit(menu2_text, menu2_text_rect)  # draw menu text in the center of the screen
-        screen.blit(selection_image, selection_image_rect)  # draw cursor
-        screen.blit(return_image, return_image_rect)  # draw return image in bottom right corner
+        if location == 4:
+            screen.blit(selection_image, selection_image_rect)  # draw cursor
+        weight_bar_width = abs(weight_progress)*width/2//100
+        weight_bar_image_use = pygame.transform.scale(weight_bar_image, (int(weight_bar_width), 50))  # scale loading bar based on selected weight
+        weight_bar_image_use_rect = weight_bar_image_use.get_rect(midleft=(200, 3/4*height))  # update loading bar position
+        screen.blit(weight_bar_image_use, weight_bar_image_use_rect)  # draw loading bar
+        weight_text,weight_rect = create_text(f"Desired weight: {weight_progress} g", (width // 2, height // 2), (0,0,0))
+        screen.blit(weight_text, weight_rect)  # draw weight text in the center
     if menu == 3:
-        screen.fill((0, 0, 255))          # clear screen (blue background)
         screen.blit(menu3_text, menu3_text_rect)  # draw menu text in the center of the screen
-        screen.blit(selection_image, selection_image_rect)  # draw cursor
         screen.blit(return_image, return_image_rect)  # draw return image in bottom right corner
+        if location == 4:
+            screen.blit(selection_image, selection_image_rect)  # draw cursor
+        hardness_bar_width = abs(hardness_progress)*width/2//50
+        hardness_bar_image_use = pygame.transform.scale(hardness_bar_image, (int(hardness_bar_width), 50))  # scale loading bar based on selected weight
+        hardness_bar_image_use_rect = hardness_bar_image_use.get_rect(midleft=(200, 3/4*height))  # update loading bar position
+        screen.blit(hardness_bar_image_use, hardness_bar_image_use_rect)  # draw loading bar
+        hardness_text,hardness_rect = create_text(f"Desired hardness: {hardness_progress}", (width // 2, height // 2), (0,0,0))
+        screen.blit(hardness_text, hardness_rect)  # draw hardness text in the center
     if menu == 4:
-        screen.fill((255, 255, 0))          # clear screen (yellow background)
         screen.blit(menu4_text, menu4_text_rect)  # draw menu text in the center of the screen
         screen.blit(selection_image, selection_image_rect)  # draw cursor
         screen.blit(yes_image, yes_image_rect)  # draw yes image
         screen.blit(no_image, no_image_rect)  # draw no image
     if menu == 5:
-        screen.fill((255, 0, 255))          # clear screen (magenta background)
         screen.blit(menu5_text, menu5_text_rect)  # draw menu text in the center of the screen
         screen.blit(selection_image, selection_image_rect)  # draw cursor
         screen.blit(return_image, return_image_rect)  # draw return image in bottom right corner    
     if menu == 6:
-        screen.fill((0, 255, 255))          # clear screen (cyan background)
         screen.blit(menu6_text, menu6_text_rect)  # draw menu text in the center of the screen
         screen.blit(selection_image, selection_image_rect)  # draw cursor
         screen.blit(return_image, return_image_rect)  # draw return image in bottom right corner
     if menu == 7:
-        screen.fill((128, 128, 128))          # clear screen (gray background)
         screen.blit(menu7_text, menu7_text_rect)  # draw menu text in the center of the screen
         screen.blit(selection_image, selection_image_rect)  # draw cursor
         screen.blit(return_image, return_image_rect)  # draw return image in bottom right corner
@@ -261,7 +293,6 @@ while running:
         elif loading_progress >= 100:
             menu = 0
             location = 0
-
     pygame.display.flip()           # update display
 
     
