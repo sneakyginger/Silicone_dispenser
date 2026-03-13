a3a6d3cfbf41331a03e192cceb25e0a231314135
sneakyginger <gregor.vanv@gmail.com>
2026-03-13 15:20:14 +0100

    ik ben confused

--- Interface/dispenser_venv/Scripts/activate.bat
+++ Interface/dispenser_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\grego\Documents\GIP\silicone_dispensor\interface\dispenser_venv
+set VIRTUAL_ENV=C:\Users\grego\Documents\GIP\Silicone_dispensor\Interface\dispenser_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
