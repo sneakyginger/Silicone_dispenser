5fac412a02b9901e388e67dd75ad933e10e2439b
sneakyginger <gregor.vanv@gmail.com>
2026-03-27 15:34:54 +0100

    fixed stepper code

--- Interface/stepper_arduino/stepper_arduino.ino
+++ Interface/stepper_arduino/stepper_arduino.ino
@@ -1,5 +1,5 @@
 const int PIN = 13;
-const float FREQ = 50.0;  // Frequency in Hz
+const float FREQ = 200;  // Frequency in Hz
 
 void setup() {
   pinMode(PIN, OUTPUT);
@@ -7,8 +7,11 @@ void setup() {
 
 void loop() {
   int halfPeriod = (1000 / FREQ) / 2;  // Half period in ms
+  //int halfPeriod = 1/FREQ/2
   digitalWrite(PIN, HIGH);
+  //delayMicroseconds()
   delay(halfPeriod);
   digitalWrite(PIN, LOW);
+  //delayMicroseconds()
   delay(halfPeriod);
 }
