61524353b145b3a569b94185cc58995366b10412
sneakyginger <gregor.vanv@gmail.com>
2026-03-27 15:59:41 +0100

    RPM Functionaliteit

--- Interface/stepper_arduino/stepper_arduino.ino
+++ Interface/stepper_arduino/stepper_arduino.ino
@@ -1,17 +1,35 @@
 const int PIN = 13;
-const float FREQ = 40.0;  // Frequency in Hz
-
+//zet hier u RPM
+const float RPM = 5;
+float steps = RPM*200/60;
+//schrijf hier hoeveel microsteps aanstaan
+int microsteps = 16;
+float halfPeriod;
+float state;
+float FREQ;
 void setup() {
   pinMode(PIN, OUTPUT);
+  FREQ = steps*microsteps;
+  halfPeriod = (1000 / FREQ) / 2;  // Half period in ms
+  state = 0;
+  if(halfPeriod <1){
+    halfPeriod = 1000000/FREQ/2;
+    state = 1;
+  }
 }
 
 void loop() {
-  int halfPeriod = (1000 / FREQ) / 2;  // Half period in ms
-  //int halfPeriod = 1/FREQ/2
-  digitalWrite(PIN, HIGH);
-  //delayMicroseconds()
-  delay(halfPeriod);
-  digitalWrite(PIN, LOW);
-  //delayMicroseconds()
-  delay(halfPeriod);
+  
+  if(state == 0){
+    digitalWrite(PIN, HIGH);
+    delay(halfPeriod);
+    digitalWrite(PIN, LOW);
+    delay(halfPeriod);
+  }else if(state == 1){
+    digitalWrite(PIN, HIGH);
+    delayMicroseconds(halfPeriod);
+    digitalWrite(PIN, LOW);
+    delayMicroseconds(halfPeriod);
+  }
+  
 }
