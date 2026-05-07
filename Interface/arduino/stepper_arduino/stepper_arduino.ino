182461fb7d91033239a8f9420efd7d9c7af9cdd0
DavidPiot1805 <david.piot.1805@gmail.com>
2026-05-07 11:22:39 +0200

    Rotor D lock

--- Interface/arduino/stepper_arduino/stepper_arduino.ino
+++ Interface/arduino/stepper_arduino/stepper_arduino.ino
@@ -24,7 +24,7 @@ void setup() {
 
 void loop() {
   digitalWrite(MF_PIN, HIGH);
-  digitalWrite(DR_PIN, LOW);
+  digitalWrite(DR_PIN, HIGH);
   if(state == 0){
     digitalWrite(PIN, HIGH);
     delay(halfPeriod);
