3bd84dcf4ac565c692002b7ba70182a81072da1b
Gregor Van Vlierberghe <gregor.vanv@gmail.com>
2026-05-08 19:06:30 +0200

    more samples

--- Interface/Weight_sensor.py
+++ Interface/Weight_sensor.py
@@ -62,7 +62,7 @@ while True:
         # print binary_string + " " + np_arr8_string
         
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val = hx.get_weight(5)
+        val = hx.get_weight(10)
         print(val)
 
         # To get weight from both channels (if you have load cells hooked up 
