27bfba9119dceab995abbb8c3384e7010cc5c469
Gregor Van Vlierberghe <gregor.vanv@gmail.com>
2026-05-11 16:48:04 +0200

    recalibrate

--- Interface/Weight_sensor.py
+++ Interface/Weight_sensor.py
@@ -39,7 +39,7 @@ In my case, the longValueWithOffset was around 114000 so my reference unit is 11
 because if I used the 114000, I'd be getting milligrams instead of grams.
 '''
 referenceUnit = 1
-referenceUnit = 2439279/165
+#referenceUnit = 2439279/165
 hx.set_reference_unit(referenceUnit)
 print("reset")
 hx.reset()
