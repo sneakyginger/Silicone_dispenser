0e2074a7c3fd1de9f05dba3844bb453c0e3ae3e4
Gregor Van Vlierberghe <gregor.vanv@gmail.com>
2026-05-11 17:13:06 +0200

    increased tare freq

--- Interface/Weight_sensor.py
+++ Interface/Weight_sensor.py
@@ -43,7 +43,7 @@ referenceUnit = 2141725/165
 hx.set_reference_unit(referenceUnit)
 print("reset")
 hx.reset()
-
+#the argument is the amount of times to measure before taring
 hx.tare(15)
 
 print("Tare done! Add weight now...")
@@ -55,28 +55,16 @@ loop_count = 0
 while True:
     try:
         loop_count += 1
-        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
+     
         
-        # np_arr8_string = hx.get_np_arr8_string()
-        # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
-        
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+        # Prints the weight. the argument is the avaraging
         val = hx.get_weight(1)
         print(val)
-
-        # To get weight from both channels (if you have load cells hooked up 
-        # to both channel A and B), do something like this
-        #val_A = hx.get_weight_A(5)
-        #val_B = hx.get_weight_B(5)
-        #print "A: %s  B: %s" % ( val_A, val_B )
 
         hx.power_down()
         hx.power_up()
         time.sleep(0.1)
-        if loop_count == 100 && val <1:
+        if loop_count == 50 and val <1:
             loop_count = 0
             hx.tare(15)
 
