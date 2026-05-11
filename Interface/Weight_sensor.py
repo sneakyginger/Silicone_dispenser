10e02625932e91afd6ce84387b7a481873fe9932
Gregor Van Vlierberghe <gregor.vanv@gmail.com>
2026-05-11 16:12:31 +0200

    fixed ref

--- Interface/Weight_sensor.py
+++ Interface/Weight_sensor.py
@@ -38,7 +38,7 @@ hx.set_reading_format("MSB", "MSB")
 In my case, the longValueWithOffset was around 114000 so my reference unit is 114,
 because if I used the 114000, I'd be getting milligrams instead of grams.
 '''
-referenceUnit = 1570300
+referenceUnit = 1570300*121.2
 hx.set_reference_unit(referenceUnit)
 print("reset")
 hx.reset()
