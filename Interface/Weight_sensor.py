42cd0efcf41af12f7f8b030176954887c6197a3a
Gregor Van Vlierberghe <gregor.vanv@gmail.com>
2026-05-14 12:49:07 +0200

    calibrated

--- Interface/Weight_sensor.py
+++ Interface/Weight_sensor.py
@@ -39,13 +39,13 @@ In my case, the longValueWithOffset was around 114000 so my reference unit is 11
 because if I used the 114000, I'd be getting milligrams instead of grams.
 '''
 referenceUnit = 1
-#sreferenceUnit = 1756016/61.71
+sreferenceUnit = 979700/61.71
 hx.set_reference_unit(referenceUnit)
 print("reset")
 hx.reset()
-time.sleep(120)
+time.sleep(2)
 #the argument is the amount of times to measure before taring
-hx.tare(15)
+hx.tare(31)
 
 print("Tare done! Add weight now...")
 
